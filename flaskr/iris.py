66b7074fa0bd4f872031d54de21febffb1dbd523
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-09-04 16:02:28 +0300

    feat: grouped average endpoint

--- flaskr/iris.py
+++ flaskr/iris.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from http import HTTPStatus
 from flask import (
     Blueprint,
@@ -22,7 +24,30 @@ def iris() -> tuple[Response, HTTPStatus]:
         records = db.execute("SELECT * FROM iris").fetchall()
         return jsonify(records), HTTPStatus.OK
     except Exception as e:
-        current_app.logger.error(f"Database error: {e}")
-        return jsonify(
-            {"error": "Internal server error"}
-        ), HTTPStatus.INTERNAL_SERVER_ERROR
+        return __db_error(e)
+
+
+@bp.route("/mean", methods=("GET",))
+def mean() -> tuple[Response, HTTPStatus]:
+    if request.method != "GET":
+        return jsonify({"error": "Invalid method"}), HTTPStatus.METHOD_NOT_ALLOWED
+
+    try:
+        db = get_db()
+        records = db.execute("SELECT * FROM iris").fetchall()
+        df = (
+            pd.DataFrame.from_records(records)
+            .drop(columns=["id"])
+            .groupby("species")
+            .mean()
+        )
+    except Exception as e:
+        return __db_error(e)
+
+    return jsonify(df.to_dict()), HTTPStatus.OK
+
+
+def __db_error(e: Exception) -> tuple[Response, HTTPStatus]:
+    """Utility method, logs and returns an internal database error"""
+    current_app.logger.error(f"Database error: {e}")
+    return jsonify({"error": "Internal server error"}), HTTPStatus.INTERNAL_SERVER_ERROR
